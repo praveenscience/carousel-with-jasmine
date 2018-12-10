22a903f34cdc24545610c4612bff6d11de7b287b
Praveen Kumar <praveenscience@gmail.com>
2018-12-10 15:33:40 +0000

    Load the images.

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function loadImages(cb) {
+	$.getJSON("https://pixabay.com/api/?key=9656065-a4094594c34f9ac14c7fc4c39&q=beautiful+landscape&image_type=photo", function (res) {
+		$.each(res.hits, function (index, image) {
+			$("ul.carousel").append(function () {
+				$li = $("<li />");
+				$li.html('<img src="' + image.webformatURL + '" alt="' + image.type + '" />');
+				return $li;
+			});
+		});
+		if (typeof cb == "function")
+			cb();
+	});
+}
--- js/tests.js
+++ js/tests.js
@@ -16,4 +16,15 @@ $(function () {
 			expect($("ul.carousel").length).toBe(1);
 		});
 	});
+	describe("Fetching Images from API", function () {
+		// This is an async request to fetch all the images from API.
+		beforeEach(function (done) {
+			loadImages(function () {
+				done();
+			});
+		});
+		it("should have at least some images", function () {
+			expect($("ul.carousel li").length).toBeGreaterThan(0);
+		});
+	});
 });
